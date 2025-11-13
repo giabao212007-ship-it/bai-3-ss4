6c84406e70ffb7f47da93ed4fa2b159269424047
giabao212007-ship-it <giabao212007@gmail.com>
2025-11-14 03:36:45 +0700

    Replace SELECT with INSERT into companyDB

--- bai3 ss4.sql
+++ bai3 ss4.sql
@@ -1,1 +1,4 @@
-SELECT * FROM companydb.companydb;
+INSERT INTO companyDB(first_name, last_name, hiredate, salary)
+VALUES ('thu','mai_xuan','2023-05-10',15000000),
+('manh','bui_duy','2023-06-15',12000000),
+('hung','nguyen_van','2023-07-01',18000000)
